bbcc8d4419cc02f17a92128fdc1b41e49e772ec4
Rolex <drolewski@gmail.com>
2023-02-06 23:23:25 +0100

    Prisma config

--- app/components/navbar/navbar.component.tsx
+++ app/components/navbar/navbar.component.tsx
@@ -8,11 +8,11 @@ type NavbarProps = {
 
 const Navbar = ({navigation}: NavbarProps) => {
     return (
-        <div className="flex flex-col items-center justify-center w-9 h-full bg-zinc-900">
+        <div className="flex flex-col items-center justify-center w-14 h-full bg-zinc-900">
             {navigation.map((nav: Navigation) =>
-                <Link className="flex items-center justify-center w-5 h-5 m-1 hover:bg-zinc-700 hover:rounded-full"
+                <Link className="flex items-center justify-center w-10 h-10 m-1 hover:bg-zinc-700 hover:rounded-full"
                       to={nav.url}>
-                    <Icon className="w-4 h-4" key={nav.icon} icon={nav.icon} description={nav.description}></Icon>
+                    <Icon className="w-6 h-6" key={nav.icon} icon={nav.icon} description={nav.description}></Icon>
                 </Link>
             )}
         </div>
